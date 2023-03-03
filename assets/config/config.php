8739b17f61fd3806ebaeec229415a59ea464be95
o Fild4 <o28o@mail.ru>
2023-03-03 20:25:16 +0800

    updates

--- assets/config/config.php
+++ assets/config/config.php
@@ -31,42 +31,10 @@ $adapterscriptlocation = '/data/data/com.termux/files/home/aksharamukha/bin/pyth
 //api-emulator
 $scroottextlocation = "/storage/emulated/0/Dhamma/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms";
 
-}
-//
-//Offline laptop or server for apache default /var/www/html e.g. Linux Mint / Ubuntu
-//
-else if ( preg_match('/Linux/', $uname)  ) {
-    $mode = 'offline';
-//$mode = 'online';
-//translator-lookup.php
-$basedir = "/var/www/html/";
-$translatorlocation = $basedir. '/o/';
-
-//opentexts.php
-$indexesfile = $basedir. "/assets/texts/text_indexes.txt";
-$scriptfile = $basedir. "/scripts/ranges.sh";
-
-$thsulocation = $basedir. '/assets/offline/tipitaka.theravada.su/';
-$thrulocation = $basedir. '/assets/offline/theravada.ru/Teaching/Canon';
-
-$linkforthsu = '/tipitaka.theravada.su/dn/';
-$linkforthru = '/theravada.ru/Teaching/Canon/Suttanta/Texts/';
-$linkforthai = "/legacy.suttacentral.net/sc/th/"; 
-$linkforthaiend = '.html';
-//extralinks.php
-$locationru = $basedir. "/assets/offline/theravada.ru/Teaching/Canon/Suttanta/Texts";
-
-//converter.php
-$adapterscriptlocation = '/data/data/com.termux/files/home/aksharamukha/bin/python3 /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/scripts/aksharamukha-adapter.py';
-
-//api-emulator
-$scroottextlocation = "/var/www/html/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms";
-      } 
-
+} else if ( preg_match('/Linux rym.from.sh/', $uname)  ) {
 //
 // ONLINE PROD
 //
-      else {
   //preg_match('/rym.from.sh/', $uname) 
   $mode = 'online';
 //translator-lookup.php
@@ -97,5 +65,35 @@ $adapterscriptlocation = '/home/a0092061/domains/find.dhamma.gift/aksharamukha/b
 //api-emulator
 $scroottextlocation = '/home/a0092061/data/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms';
 // include_once('../config/config.php');
-      }
+      } else if ( preg_match('/Linux/', $uname)  ) {     
+  //
+//Offline laptop or server for apache default /var/www/html e.g. Linux Mint / Ubuntu
+//
+
+      $mode = 'offline';
+//$mode = 'online';
+//translator-lookup.php
+$basedir = "/var/www/html/";
+$translatorlocation = $basedir. '/o/';
+
+//opentexts.php
+$indexesfile = $basedir. "/assets/texts/text_indexes.txt";
+$scriptfile = $basedir. "/scripts/ranges.sh";
+
+$thsulocation = $basedir. '/assets/offline/tipitaka.theravada.su/';
+$thrulocation = $basedir. '/assets/offline/theravada.ru/Teaching/Canon';
+
+$linkforthsu = '/tipitaka.theravada.su/dn/';
+$linkforthru = '/theravada.ru/Teaching/Canon/Suttanta/Texts/';
+$linkforthai = "/legacy.suttacentral.net/sc/th/"; 
+$linkforthaiend = '.html';
+//extralinks.php
+$locationru = $basedir. "/assets/offline/theravada.ru/Teaching/Canon/Suttanta/Texts";
+
+//converter.php
+$adapterscriptlocation = '/data/data/com.termux/files/home/aksharamukha/bin/python3 /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/scripts/aksharamukha-adapter.py';
+
+//api-emulator
+$scroottextlocation = "/var/www/html/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms";
+      } 
       ?>

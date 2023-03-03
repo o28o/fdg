e9d4bcaac2e7a3ac6b9b252852523a00ee0d084e
o Fild4 <o28o@mail.ru>
2023-03-03 19:42:27 +0800

    updates

--- assets/config/config.php
+++ assets/config/config.php
@@ -2,10 +2,13 @@
 
 $uname = shell_exec("uname -a"); 
 
-  
-      if ( preg_match('/Android/', $uname)  ) {
+//
+//OFFLINE ANDROID
+//
+if ( preg_match('/Android/', $uname)  ) {
     $mode = 'offline';
 //$mode = 'online';
+
 //translator-lookup.php
 $basedir = "/data/data/com.termux/files/usr/share/apache2/default-site/htdocs";
 $translatorlocation = $basedir. '/o/';
@@ -30,7 +33,42 @@ $adapterscriptlocation = '/data/data/com.termux/files/home/aksharamukha/bin/pyth
 //api-emulator
 $scroottextlocation = "/storage/emulated/0/Dhamma/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms";
 
-      } else {
+}
+//
+//Offline for apache default /var/www/html e.g. Linux Mint / Ubuntu
+//
+else if ( preg_match('/Linux/', $uname)  ) {
+    $mode = 'offline';
+//$mode = 'online';
+//translator-lookup.php
+$basedir = "/var/www/html/";
+$translatorlocation = $basedir. '/o/';
+
+//opentexts.php
+$indexesfile = $basedir. "/assets/texts/text_indexes.txt";
+$scriptfile = $basedir. "/scripts/ranges.sh";
+
+$thsulocation = $basedir. '/assets/offline/tipitaka.theravada.su/';
+$thrulocation = $basedir. '/assets/offline/theravada.ru/Teaching/Canon';
+
+$linkforthsu = '/tipitaka.theravada.su/dn/';
+$linkforthru = '/theravada.ru/Teaching/Canon/Suttanta/Texts/';
+$linkforthai = "/legacy.suttacentral.net/sc/th/"; 
+$linkforthaiend = '.html';
+//extralinks.php
+$locationru = $basedir. "/assets/offline/theravada.ru/Teaching/Canon/Suttanta/Texts";
+
+//converter.php
+$adapterscriptlocation = '/data/data/com.termux/files/home/aksharamukha/bin/python3 /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/scripts/aksharamukha-adapter.py';
+
+//api-emulator
+$scroottextlocation = "/var/www/html/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms";
+      } 
+
+//
+// ONLINE PROD
+//
+      else {
   //preg_match('/rym.from.sh/', $uname) 
   $mode = 'online';
 //translator-lookup.php

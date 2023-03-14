a29c44ba6b615ad266d8263e70ffe6d1058df0ae
o Fild4 <o28o@mail.ru>
2023-03-14 20:35:00 +0800

    updates

--- config/config.php
+++ config/config.php
@@ -9,17 +9,11 @@ if ( preg_match('/Android/', $uname)  ) {
 
 $mainpagethrulink = "/theravada.ru/Teaching/Canon/Suttanta/all-suttas-list.htm";
       $mainpagethsulink = '/tipitaka.theravada.su/dn_toc_thsu.html' ;
-//translator-lookup.php
+      
 $basedir = "/data/data/com.termux/files/usr/share/apache2/default-site/htdocs";
-$translatorlocation = $basedir. '/o/';
 
-//opentexts.php
-$indexesfile = $basedir. "/assets/texts/text_indexes.txt";
-$scriptfile = $basedir. "/scripts/ranges.sh";
 
-$thsulocation = $basedir. '/assets/offline/tipitaka.theravada.su/';
-$thrulocation = $basedir. '/assets/offline/theravada.ru/Teaching/Canon';
-$bwlocation = $basedir. '/assets/offline/bw';
+
 $linkforthsu = '/tipitaka.theravada.su/dn/';
 $linkforthru = '/theravada.ru/Teaching/Canon/Suttanta/Texts/';
 $linkforthai = "/legacy.suttacentral.net/sc/th/"; 
@@ -41,17 +35,9 @@ $scroottextlocation = "/storage/emulated/0/Dhamma/suttacentral.net/sc-data/sc_bi
   $mode = 'online';
   $mainpagethrulink = "https://theravada.ru/Teaching/Canon/Suttanta/all-suttas-list.htm";
   $mainpagethsulink = 'https://tipitaka.theravada.su/toc/translations/1097' ;
-//translator-lookup.php
+  
 $basedir = '/home/a0092061/domains/find.dhamma.gift/public_html';
-$translatorlocation = $basedir. '/o/';
 
-//opentexts.php
-$indexesfile = $basedir. '/assets/texts/text_indexes.txt';
-$scriptfile = $basedir. '/scripts/ranges.sh';
-
-$thsulocation = $basedir. '/assets/offline/tipitaka.theravada.su/';
-$thrulocation = $basedir. '/assets/offline/theravada.ru/Teaching/Canon';
-$bwlocation = $basedir. '/assets/offline/bw';
 $linkforthsu = 'https://tipitaka.theravada.su/dn/';
 $linkforthru = 'https://theravada.ru/Teaching/Canon/Suttanta/Texts/';
 $linkforthai = "https://suttacentral.net/"; 
@@ -79,17 +65,9 @@ $scroottextlocation = '/home/a0092061/data/suttacentral.net/sc-data/sc_bilara_da
  $mainpagethrulink = "/theravada.ru/Teaching/Canon/Suttanta/all-suttas-list.htm";
       $mainpagethsulink = '/tipitaka.theravada.su/dn_toc_thsu.html' ;
 //$mode = 'online';
-//translator-lookup.php
+
 $basedir = "/var/www/html/";
-$translatorlocation = $basedir. '/o/';
 
-//opentexts.php
-$indexesfile = $basedir. "/assets/texts/text_indexes.txt";
-$scriptfile = $basedir. "/scripts/ranges.sh";
-
-$thsulocation = $basedir. '/assets/offline/tipitaka.theravada.su/';
-$thrulocation = $basedir. '/assets/offline/theravada.ru/Teaching/Canon';
-$bwlocation = $basedir. '/assets/offline/bw';
 $linkforthsu = '/tipitaka.theravada.su/dn/';
 $linkforthru = '/theravada.ru/Teaching/Canon/Suttanta/Texts/';
 $linkforthai = "/legacy.suttacentral.net/sc/th/"; 

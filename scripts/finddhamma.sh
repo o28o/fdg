fdc25b2c2a8f042c3b14ad512b9952158fa81bb2
o Fild4 <o28o@mail.ru>
2023-03-17 09:55:41 +0800

    updates

--- scripts/finddhamma.sh
+++ scripts/finddhamma.sh
@@ -275,7 +275,7 @@ grep -E "($onltextindex)" $tmponl
 }
 fileprefix=${fileprefix}-onl
 fortitle="${fortitle} Matching Mode"
-
+maxmatchesbg=1000000000000000000000000000
 else 
 function grepbasefile {
 nice -$nicevalue grep -E -Ri${grepvar}${grepgenparam} "$pattern" $suttapath/$pali_or_lang --exclude-dir={$sutta,$abhi,$vin,xplayground,name,site} --exclude-dir={ab,bv,cnd,cp,ja,kp,mil,mnd,ne,pe,ps,pv,tha-ap,thi-ap,vv,thag,thig,snp,dhp,iti,ud} 

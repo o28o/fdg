7fa19c870b59e5a5ee1dcd3b6e639273fdfc10b5
o Fild4 <o28o@mail.ru>
2023-04-11 15:35:28 +0700

    updates

--- sc/api.php
+++ sc/api.php
@@ -9,12 +9,13 @@ function getAround($fromjs,$type) {
   $output = shell_exec("slug=`echo $fromjs | awk -F'/' '{print \$NF}'` ; dirtolist=`echo $fromjs | awk -F'/' '{ var=NF-1 ; for (i=1;i<=var;i++) printf \$i\"/\"}'` ; 
   next=`ls $location/\$dirtolist | sort -V | grep -{$type}1 \${slug}_ | grep -v \${slug}_ ` 
   nextslug=`echo \$next | awk -F'_' '{print \$1}'`
-  textname=`head -n7 $location/\$dirtolist/\$next | grep -E \":0\..*(sutt|–|~|pada)\" | awk -F'\": \"' '{print \$2}' | sed 's@[0-9 \.,\"]*@@g'` 
+  textname=`head -n7 $location/\$dirtolist/\$next | grep -E \":0\..*(sutt|pada)\" | awk -F'\": \"' '{print \$2}' | sed 's@[0-9 \.,\"]*@@g'` 
   [[ \$slug == \$next ]] && echo -n \"\" || echo -n \$nextslug \$textname
   
   ");
 return $output;
 }
 //$fromjs = "sutta/dn/dn34";
+// |–|~ 
 echo getAround($_GET['fromjs'],$_GET['type']);
 ?>

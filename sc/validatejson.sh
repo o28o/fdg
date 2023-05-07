9dcf72070ea6b1e02e4988faae0f94452197c429
o Fild4 <o28o@mail.ru>
2023-05-07 08:21:24 +0700

    updates

--- sc/validatejson.sh
+++ sc/validatejson.sh
@@ -4,10 +4,8 @@ cd $apachesitepath/assets/texts/sutta
 for i in `find . -type f -name "*.json" `; do  
 cat $i | jq 1>/dev/null 
 if [[ $? != 0 ]]; then 
-
-
-
 echo; 
-echo "error in $i"; echo; fi ; done
+echo "</br>
+error in $i"; echo; fi ; done
 
 # json_pp -t null

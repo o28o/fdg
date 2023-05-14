6b63d770ad6eebcd70a5efe4c6c600bba5d0a02a
o Fild4 <o28o@mail.ru>
2023-05-14 09:02:08 +0700

    updates

--- scripts/multilang-search.php
+++ scripts/multilang-search.php
@@ -119,6 +119,18 @@ $outforjs .= $output . "<br>";
 
 
 		$check = ru2lat( $output );
+		
+		
+if ( preg_match('/(|-en)/', $p ) && ( preg_match('/(-not-in-|-net-v-)/', $check) ) && ( $p != "-vin" ) && ( $p != "-def" ))  {
+$output = shell_exec("bash ./scripts/finddhamma.sh $outputlang $la -vin $extra $string");
+//                                                          		echo "<p>$output</p>";
+      $output = trim(preg_replace('/\s\s+/', ' ', $output));	
+$outforjs .= $output . "<br>"; 
+
+}	
+
+
+		$check = ru2lat( $output );
 
 if ( preg_match('/(|-en)/', $p ) && ( preg_match('/(-not-in-|-net-v-)/', $check) ) && ( $p != "-vin" ) && ( $p != "-def" ))  {
 $output = shell_exec("bash ./scripts/finddhamma.sh $outputlang $la -en $extra $string");

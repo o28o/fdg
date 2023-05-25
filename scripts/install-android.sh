98b926e3dc902c516f0ebad3b10485f25fca901a
o Fild4 <o28o@mail.ru>
2023-05-25 19:24:19 -0400

    updates

--- scripts/install-android.sh
+++ scripts/install-android.sh
@@ -260,3 +260,7 @@ wget \
      --restrict-file-names=windows \
      --no-parent \
           http://xn--80aaaglc1fo1a.xn--p1ai/palicanon
+
+
+wget --recursive --no-clobber --page-requisites --html-extension --convert-links --no-parent --no-host-directories --reject=pdf https://example.com
+

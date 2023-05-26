9e267674bfad491916bb60d8c8a27e7968cb53c0
o Fild4 <o28o@mail.ru>
2023-05-26 09:14:30 -0400

    updates

--- sc/api.php
+++ sc/api.php
@@ -1,35 +1,51 @@
 <?php
 // test request
 // curl http://localhost:8080/sc/api.php?fromjs=sutta/dn/dn22&type=A
-error_reporting(E_ERROR | E_PARSE);
-header("Content-Type:text/plain");
+//error_reporting(E_ERROR | E_PARSE);
+//header("Content-Type:text/plain");
+var_dump($_GET);
+$fromjs = $_GET['fromjs'] ?? '';
+$type = $_GET['look'] ?? '';
+echo "look=$type";
 function getAround($fromjs,$type) {
-  include_once('../config/config.php');
+ include_once('../config/config.php');
   $location = $scroottextlocation;
-  $output = shell_exec("slug=`echo $fromjs | awk -F'/' '{print \$NF}'` ; dirtolist=`echo $fromjs | awk -F'/' '{ var=NF-1 ; for (i=1;i<=var;i++) printf \$i\"/\"}'` ; 
-  next=`ls $location/\$dirtolist | sort -V | grep -{$type}1 \${slug}_ | grep -v \${slug}_ ` 
-  nextslug=`echo \$next | awk -F'_' '{print \$1}'`
-  textname=`head -n7 $location/\$dirtolist/\$next 2>/dev/null | grep -E \":0\..*(sutt|pada)\" | awk -F'\": \"' '{print \$2}' | sed 's@[0-9 \.,\"]*@@g'` 
   
-  if [[ \$textname == '' ]]
-  then
- textname=''
-  fi
-  
-  if [[ \$slug == \$next ]]
-  then
-  echo -n ''
-  else
-  echo -n \$nextslug \$textname
-  fi
-  ");
-return $output;
+$parts = explode('/', $fromjs);
+$slug = end($parts);
+array_pop($parts);
+$otherFields = $parts;
+$dirtolist = implode('/', $otherFields);
+
+$command = "ls $location/$dirtolist | sort -V | sort -V | grep -{$type}1 {$slug}_ | grep -v {$slug}_";
+echo $command;
+$next = shell_exec($command);
+
+$nextslug = preg_replace('/_.*$/', '', $next);
+$nextslug = str_replace(PHP_EOL, '', $nextslug);
+$textname = shell_exec("grep -m1 -E \":0\..*(sutt|pada)\" $location/$dirtolist/$next");
+
+$pattern = '/"([^"]+)"\s*,/'; // Matches a string enclosed in double quotes followed by a comma (with optional whitespace)
+$matches = array();
+
+if (!empty($textname)) {
+if (preg_match($pattern, $textname, $matches)) {
+    $extractedValue = $matches[1];
+    
+} else {
+    $extractedValue = "empty";
 }
-$fromjs = "sutta/an/an1.1-10";
+} else {
+  return "$nextslug";
+}
+return "$nextslug $extractedValue";
+
+}
+//$fromjs = "sutta/an/an1/an1.1-10";
 //$fromjs = "sutta/dn/dn31";
-$type = "B";
+//$type = "A";
 // |–|~ 
+//echo getAround($fromjs,$type);
 echo getAround($fromjs,$type);
 
-//echo getAround($_GET['fromjs'],$_GET['type']);
 ?>

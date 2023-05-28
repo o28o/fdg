a8cec80c08fb0c786bd241644c86a98fa8f5c0d6
o lt <o28o@laptop.o>
2023-05-28 01:00:20 -0400

    update from server

---
+++ json.php
@@ -0,0 +1,20 @@
+<?php
+
+$jsonString = file_get_contents('config/ru.json');
+$data = json_decode($jsonString, true);
+
+$lang = $data['lang'];
+$htmllang = $data['htmllang'];
+$mainpage = $data['mainpage'];
+$mainscpage = $data['mainscpage'];
+$searchcaption = $data['searchcaption'];
+$metadesc = $data['metadesc'];
+
+// You can use the variables here...
+echo $lang;
+echo $htmllang;
+echo $ogshare;
+// and so on...
+
+
+?>

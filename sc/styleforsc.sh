857ae458ec8e28a0a3efb545feae2101ecb83710
o lt <o28o@laptop.o>
2023-05-28 15:17:46 -0400

    update from server

--- sc/styleforsc.sh
+++ sc/styleforsc.sh
@@ -4,8 +4,9 @@ for i in `find . -type f -name "*.json" `; do
 if grep -q '\.\.\.' $i
 then
 echo -n "fixing ellipsis in $i"
-cat $i | sed 's@\.\.\.@…@g' | sed -i 's@……@… …@g' | sed 's@  …@ …@g' > tmp
-mv tmp $i 
+sed -i 's@\.\.\.@…@g' $i
+sed -i 's@……@… …@g' $i
+sed -i 's@  …@ …@g' $i
 echo " done <br>"
 fi
 done

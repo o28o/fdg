6214544384ba91b96570d9604c3115e94d309491
o lt <o28o@laptop.o>
2023-06-01 14:12:32 -0400

    update from server

--- sc/translator-lookup.php
+++ sc/translator-lookup.php
@@ -8,7 +8,13 @@ header("Content-Type:text/plain");
 function translatorLookup($fromjs) {
   include_once('../config/config.php');
   
-  $output = shell_exec("ls $translatorlocation/{$fromjs}_* | awk -F'-' '{print \$NF}' | sed 's@.json@@g' ");
+  
+  $output = shell_exec("translator=o 
+  filename=$translatorlocation/{$fromjs}_translation-ru-\$translator.json
+  if [[ -s "\$filename" ]]; then
+    echo \$translator
+else
+  ls $translatorlocation/{$fromjs}_translation-ru-*.json | awk -F'-' '{print \$NF}' | sed 's@.json@@g' | head -n1 ");
  $result = str_replace(PHP_EOL, '', $output);
 return $result;
 }

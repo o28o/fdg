958e9661026ca6e6394cde2489595e00a393f15a
o lt <o28o@laptop.o>
2023-06-12 21:41:06 -0400

    update from server

--- scripts/finddhamma.sh
+++ scripts/finddhamma.sh
@@ -753,7 +753,7 @@ do
 		for f in $roottext $translation $variant
         do     
 		quote=`nice -$nicevalue grep -E -A${linesafter} -iE "${i}(:|[^0-9]|$)" $f | grep -v "^--$" | removeindex | clearsed | awk '{print substr($0, index($0, $2))}'  | highlightpattern `
-[[ "$f" == *"root"* ]] && echo "<span class=\"pli-lang inputscript-ISOPali\" lang=\"pi\">$quote <a target=_blank href=\"$linkgeneral\#$i\">_</a></span><br class=\"btwntrn\">" || echo "<span class=\"eng-lang text-muted font-weight-light\" lang=\"en\">$quote</span>"
+[[ "$f" == *"root"* ]] && echo "<span class=\"pli-lang inputscript-ISOPali\" lang=\"pi\">$quote <a target=_blank href=\"$linkgeneral#$i\">_</a></span><br class=\"btwntrn\">" || echo "<span class=\"eng-lang text-muted font-weight-light\" lang=\"en\">$quote</span>"
 done
 echo '<br class="styled">'
 done | tohtml 

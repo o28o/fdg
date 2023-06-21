9324ae739433a1bb703b0669b6157c6173802294
ofold4 <o28o@mail.ru>
2023-06-21 10:06:22 -0400

    updates

--- config/translate.php
+++ config/translate.php
@@ -74,6 +74,7 @@ $regexMemo = '<h5>Памятка по RegEx</h5>
           
   <!--   <strong>-onl "(X|Y|...)"</strong> - найти тексты содержащие только все совпадения X, Y ... и т.д.<br> -->
        <strong>-la2 X</strong> - искать X и добавить в результаты 2 следующие строки после строки с X<br>
+    <strong>-lb1 X</strong> - искать X и добавить в результаты 1 предыдущую строку перед строкой с X<br>
      <strong>X -exc Y</strong> - искать X, исключить Y<br>
     <strong>X -exc "Y(ti|nti)"</strong> - искать X, исключить Y с окончаниями на "ti" и "nti"<br> 
          <strong>\\\\bX</strong> - начало или <strong>Y\\\\b</strong> конец слова<br>
@@ -425,6 +426,7 @@ $regexMemo = ' <h5>RegEx Memo</h5>
           <p style="text-align: left;">
      <!--  <strong>-onl "(X|Y|...)"</strong> - find texts containing only all of the X, Y ... etc patterns<br> -->
      <strong>-la1 X</strong> - search for X, add 1 next line to output after lines containing X<br>
+         <strong>-lb2 X</strong> - search for X, add 2 previous lines to output before lines containing X<br>
        <strong>X -exc Y</strong> - search for X, exclude Y<br>
            <strong>X -exc "Y(ti|nti)"</strong> - search for X, exclude Y ending with "ti" and "nti"<br> 
        <strong>\\\\bX</strong> - beginning of the word or <strong>Y\\\\b</strong> end<br>

3086162419822acac09360ef6b546af3cc3da9f7
ofold4 <o28o@mail.ru>
2023-06-26 17:14:50 -0400

    updates

--- index.php
+++ index.php
@@ -154,10 +154,9 @@ include 'scripts/opentexts.php';
   
          <div style="max-width: 300px;" class="my-2"> 
          
-<div class="align-items-center form-check-inline mt-2">
+<div class="align-items-center form-check-inline mt-1">
   <div class="mb-2"><button class="btn btn-secondary rounded-pill insert-letter" data-letter="ā" autocomplete="off">ā</button>
   <button class="btn btn-secondary rounded-pill insert-letter" data-letter="ṁ" autocomplete="off">ṁ</button>
- 
   <button class="btn btn-secondary rounded-pill insert-letter" data-letter="ñ" autocomplete="off">ñ</button>
  <button class="btn btn-secondary rounded-pill insert-letter" data-letter="ṭ" autocomplete="off">ṭ</button>
   <button class="btn btn-secondary rounded-pill insert-letter" data-letter="ī" autocomplete="off">ī</button> </div>

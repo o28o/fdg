99fe4b232e1c6e49a3524da60596571bdd538d78
o-kv <kvserv@o28o.zhh>
2023-06-28 20:41:21 +0600

    update from server

--- index.php
+++ index.php
@@ -946,15 +946,129 @@ $mainpagesclink = 'https://sc.dhamma.gift' . $mainpage ;
 
 
 <script>
+$.ajax({
+  url: "/assets/texts/sutta_words.txt",
+  dataType: "text",
+  success: function(data) {
+
+    var accentMap = {
+      "ā": "a",
+      "ī": "i",
+      "ū": "u",
+      "ḍ": "d",
+      "ḷ": "l",
+      "ṁ": "n",
+      "ṁ": "m",
+      "ṅ": "n",
+      "ṇ": "n",
+      "ṭ": "t",
+      "ñ": "n",
+      "ss": "s",
+      "aa": "a",
+      "ii": "i",
+      "uu": "u",
+      "dd": "d",
+      "kk": "k",
+      "ḍḍ": "d",
+      "ḷḷ": "l",
+      "ṁṁ": "n",
+      "ṅṅ": "n",
+      "ññ": "n",
+      "ṇṇ": "n",
+      "ṭṭ": "t",
+      "ss": "s",
+      "cc": "c",
+      "pp": "p",
+      "ch": "c",
+      "kh": "k",
+      "ph": "p",
+      "th": "t",
+      "ṭh": "t",
+      "dh": "d" 
+    };
+
+    var normalize = function(term) {
+      var ret = "";
+      for (var i = 0; i < term.length; i++) {
+        var doubleLetter = term.substr(i, 2);
+        if (accentMap[doubleLetter]) {
+          ret += accentMap[doubleLetter];
+          i++; // пропустить следующий символ
+        } else {
+          ret += accentMap[term.charAt(i)] || term.charAt(i);
+        }
+      }
+      return ret;
+    };
+
+    var allWords = data.split('\n');
+
+    $("#paliauto").autocomplete({
+      position: {
+        my: "left bottom",
+        at: "left top",
+        collision: "flip"
+      },
+      minLength: 0,
+      multiple: " ",
+      source: function(request, response) {
+        var terms = request.term.split(" ");
+        var lastTerm = terms.pop().trim();
+        var otherMinLength = 3;
+
+        if (lastTerm.length < otherMinLength) {
+          response([]);
+          return;
+        }
+
+        var re = $.ui.autocomplete.escapeRegex(lastTerm);
+        var matchbeginonly = new RegExp("^" + re, "i");
+        var matchall = new RegExp(re.replace(/([a-z])\1/gi, "$1$1"), "i");
+
+        var listBeginOnly = $.grep(allWords, function(value) {
+          value = value.label || value.value || value;
+          var results = matchbeginonly.test(value) || matchbeginonly.test(normalize(value));
+          return results;
+        });
+
+        var listAll = $.grep(allWords, function(value) {
+          value = value.label || value.value || value;
+          var results = matchall.test(value) || matchall.test(normalize(value));
+          return results;
+        });
+
+        listAll = listAll.filter(function(el) {
+          return !listBeginOnly.includes(el);
+        });
+
+        // Ограничение количества подсказок до 10
+        var maxRecord = 1000;
+        var resultList = listBeginOnly.concat(listAll).slice(0, maxRecord);
+
+        response(resultList);
+      },
+      focus: function(event, ui) {
+        // Удаляем автоматическое введение при наведении мыши
+        return false;
+      },
+      select: function(event, ui) {
+        var terms = this.value.split(" ");
+        terms.pop();
+        terms.push(ui.item.value);
+        this.value = terms.join(" ");
+        return false;
+      }
+    }).autocomplete("widget").addClass("fixed-height");
+  }
+});
 
 </script>
-<script src="/assets/js/bootstrap.bundle.5.13.min.js"></script> 
 
-<script src="/assets/js/autopali.js"></script>
+<script src="/assets/js/bootstrap.bundle.5.13.min.js"></script> 
 
 <!-- <script type="module" src="/assets/js/autopali.js"></script> -->
 	  
-      <script async src="/assets/js/randPlaceholder.js"></script>
+      <script src="/assets/js/randPlaceholder.js"></script>
 <script>
   randCallToAction();
   randPlaceholderOnMain();

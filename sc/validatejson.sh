011d20a69f0de5429760ff6119a6e1c8ce99152e
ofold4 <o28o@mail.ru>
2023-08-18 23:48:18 +0300

    updates

--- sc/validatejson.sh
+++ sc/validatejson.sh
@@ -1,5 +1,6 @@
 source ../config/script_config.sh --source-only
 cd $apachesitepath/assets/texts/sutta
+
 for i in `find . -type f -name "*.json" `; do
 cat $i | jq 1>/dev/null
 if [[ $? != 0 ]]; then
@@ -7,5 +8,13 @@ echo;
 echo "</br>
 error in $i"; echo; fi ; done
 
+cd $apachesitepath/assets/texts/vinaya
+
+for i in `find . -type f -name "*.json" `; do
+cat $i | jq 1>/dev/null
+if [[ $? != 0 ]]; then
+echo;
+echo "</br>
+error in $i"; echo; fi ; done
 # json_pp -t null
 

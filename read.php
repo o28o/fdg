8d6ceacf04e2e3922b2e7d0969351b016fe706b7
ofold4 <o28o@mail.ru>
2023-10-22 06:02:17 +0530

    updates

--- read.php
+++ read.php
@@ -18608,7 +18608,7 @@ include $basedir . "/assets/texts/vinaya/bipm.php";
 <?php echo $contaccalltoaction; ?>
 </p>
 <a  target="_blank"  class="btn btn-outline-light btn-social mx-1" href="https://github.com/o28o/fdg#readme"><i class="fa-brands fa-github"></i></a>
-<a  target="_blank"  class="btn btn-outline-light btn-social mx-1" href="mailto:o@dhamma.gift"><i class="fa-solid fa-at"></i></a>
+<a  target="_blank"  class="btn btn-outline-light btn-social mx-1" href="mailto:agiftofdhamma@gmail.com"><i class="fa-solid fa-at"></i></a>
 <a href="https://m.youtube.com/channel/UCoyL5T0wMubqrj4OnKVOlMw" class="btn btn-outline-light btn-social mx-1" title="YouTube" target="_blank" rel="nofollow"><i class="fa-brands fa-youtube"></i></a>
                     </div>
                 </div>

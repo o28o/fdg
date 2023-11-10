a52659394248f0e5d3dbee5686936e9f2fa3a75d
ofold4 <o28o@mail.ru>
2023-11-10 19:47:37 +0700

    updates

--- scripts/finddhamma.sh
+++ scripts/finddhamma.sh
@@ -1038,7 +1038,7 @@ grepbasefile | grep -v "^--$" | grepexclude | clearsed | sort -Vf > $basefile
 if [[ "$@" == *"-nm"* ]] 
 then
 cp $basefile bfl
-topmatchesintexts=`cat $basefile | awk '{print $1}' | uniq -c | sort -r | head -n$numbersmatches | awk '{print $2}'`
+topmatchesintexts=`cat $basefile | awk '{print $1}' | uniq -c | LC_ALL=C sort -r | head -n$numbersmatches | awk '{print $2}'`
 for i in $topmatchesintexts
 do
 grep $i $basefile

3956fa9342ce2aea087b95305527476ce6ea75cd
ofold4 <o28o@mail.ru>
2023-11-19 11:05:31 +0700

    updates

--- index.php
+++ index.php
@@ -74,6 +74,7 @@ include 'scripts/opentexts.php';
 <li class="nav-item mb-0 mx-lg-2"><p><a class="py-1 text-decoration-none px-0 px-lg-1 rounded link-light" href="/">En</a> 
 <a class="link-light text-decoration-none py-1 px-0 px-lg-1 rounded" href="/ru/">Ru</a>
 </p></li><div class="form-check form-switch input-group-append">
+  <!-- <i class="nav-item fa-solid fa-circle-half-stroke"></i> -->
 <input type="checkbox" class="form-check-input" id="darkSwitch">
 </div>
        </ul>

8267a0410ff6f717e739c067a9d4710612502f14
ofold4 <o28o@mail.ru>
2023-11-25 04:39:24 +0700

    updates

--- index.php
+++ index.php
@@ -1104,6 +1104,7 @@ $.ajax({
         collision: "flip"
       },
       minLength: 0,
+      multiple: /[\s\*]/, // изменение регулярного выражения для разделения по пробелу или звездочке
       source: function(request, response) {
         var terms = request.term.split(/[\s\*]/); // изменение регулярного выражения для разделения по пробелу или звездочке
         var lastTerm = terms.pop().trim();
@@ -1134,9 +1135,10 @@ $.ajax({
           return !listBeginOnly.includes(el);
         });
 
-        // Ограничение количества подсказок до 1000
+        // Ограничение количества подсказок до 10
         var maxRecord = 1000;
         var resultList = listBeginOnly.concat(listAll).slice(0, maxRecord);
+
         response(resultList);
       },
       focus: function(event, ui) {
@@ -1144,12 +1146,22 @@ $.ajax({
         return false;
       },
       select: function(event, ui) {
-        var terms = this.value.split(/[\s\*]/); // изменение регулярного выражения для разделения по пробелу или звездочке
-        terms.pop();
-        terms.push(ui.item.value);
-        this.value = terms.join(" ");
-        return false;
-      }
+  var terms = this.value.split(/([\s\*])/);
+  terms.pop();
+  terms.push(ui.item.value);
+  
+  for (var i = 1; i < terms.length; i += 2) {
+    if (terms[i] === "*") {
+      terms[i] = "*";
+    } else {
+      terms[i] = " ";
+    }
+  }
+
+  this.value = terms.join("");
+  return false;
+}
+
     }).autocomplete("widget").addClass("fixed-height");
   }
 });

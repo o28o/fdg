47dab56b89559c4ee9e9339c2b27c7fb275aad9d
o28 <o-flip5@flip5>
2024-01-11 05:24:14 +0700

    updates

--- scripts/finddhamma.sh
+++ scripts/finddhamma.sh
@@ -1300,7 +1300,7 @@ if [[ "$@" == *"-nm"* ]]; then
 fortitle="TOP-$numbersmatches ${fortitle^}"
 fi
 
-
+#<img src='/assets/svg/regular-star.svg' class='starsvg'></img>
 echo -n "<!-- begin $userpattern --> 
 <tr><td><a class=\"outlink\" href=\"./result/${table}\">${userpattern}</a></td><td><label class='star-checkbox'><input type='checkbox' data-index=\"${table}\"/><i class='fa-regular fa-star'></i></label></td><td>$textsqnty</td><td>$matchqnty</td><td><a class=\"outlink\" href=\"./result/${tempfilewords}\">$uniqwordtotal</a></td><td>${fortitle^}</td><td>$language</td><td class=\"daterow\">$dateforhist</td><td>`ls -lh ${table} | awk '{print  $5}'`</td><td>" >> $history
 #}'`</td><td><label  class='custom-checkbox'><input type='checkbox' class='star-checkbox' data-index='1'/><i class='fa-solid fa-star glyphicon glyphicon-star-empty'></i><i class='fa-solid fa-star glyphicon glyphicon-star'></i></label></td><td>"

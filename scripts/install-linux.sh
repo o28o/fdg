1639ed7087cb5b7143a6b3768109918d327a4ae5
o28 <o-flip5@flip5>
2024-01-13 16:13:04 +0700

    updates

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -20,8 +20,4 @@ sudo systemctl restart apache2
 #check this or similar article for "how to check apache and php installation"
 # https://www.vultr.com/docs/install-linux-apache-mysql-and-php-lamp-on-ubuntu-20-04-lts/
 
-
-#vmtouch -dlvt ./suttacentral.net/sc-data/html_text/ru ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/ ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/vinaya/
-
-
-#vmtouch -vtdl ./suttacentral.net/sc-data/html_text/ru ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/ ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/vinaya/ ./theravada.ru/ ./tipitaka.theravada.su/ ./result/
+#vmtouch -vfldt ./suttacentral.net/sc-data/html_text/ru ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/ ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/vinaya/ ./theravada.ru/Teaching/Canon/Suttanta/ ./tipitaka.theravada.su/ ./result/ ./assets/js/ ./assets/css/ ./assets/texts/ ./index.php ./read.php ./sc/ ./scripts/ 

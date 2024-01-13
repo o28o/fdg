59f635fc9cb7aef251fe1179ca363dcb611dbd32
o28 <o-flip5@flip5>
2024-01-13 16:12:03 +0700

    updates

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -19,3 +19,9 @@ sudo systemctl restart apache2
 
 #check this or similar article for "how to check apache and php installation"
 # https://www.vultr.com/docs/install-linux-apache-mysql-and-php-lamp-on-ubuntu-20-04-lts/
+
+
+#vmtouch -dlvt ./suttacentral.net/sc-data/html_text/ru ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/ ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/vinaya/
+
+
+#vmtouch -vtdl ./suttacentral.net/sc-data/html_text/ru ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/ ./suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/vinaya/ ./theravada.ru/ ./tipitaka.theravada.su/ ./result/

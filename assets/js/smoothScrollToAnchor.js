147f219943e79b2e9d8b4f22026baa7af1b7ac99
o28 <o-flip5@flip5>
2024-01-20 10:58:06 +0700

    updates

--- assets/js/smoothScrollToAnchor.js
+++ assets/js/smoothScrollToAnchor.js
@@ -1,13 +1,15 @@
 //smooth scroll to anchor
     window.addEventListener('DOMContentLoaded', function() {
       var hash = window.location.hash;
+      const timeout = window.location.hostname === 'localhost' ? 500 : 2500;
+
       if (hash) {
         setTimeout(function() {
           var element = document.getElementById(hash.substring(1));
           if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
           }
-        }, 1500);
+        }, timeout);
       }
     });
 

3384c649dcf77e26b3cf0ea715c514f660e8009b
a9 <a9@sams.com>
2024-01-24 06:12:21 +0700

    updates

--- index.php
+++ index.php
@@ -240,7 +240,7 @@ $(document).ready(function() {
      <div class="modal fade" id="copyPopup" tabindex="-1" role="dialog" aria-labelledby="copyPopupLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-body">
+            <div class="modal-body text-muted">
                  Done
             </div>
         </div>

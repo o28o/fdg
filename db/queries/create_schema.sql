6f2344a9bd638ba4c2c7333dd859f75d78b387bb
a9 <a9@sams.com>
2024-02-07 19:36:27 +0700

    updates

--- db/queries/create_schema.sql
+++ db/queries/create_schema.sql
@@ -1,61 +1,59 @@
 #create schema
 
+DROP TABLE IF EXISTS texts_pi;
 
-fdgdb=fdg-db.db 
-for table in 
-
-DROP TABLE IF EXISTS sutta_pi;
-CREATE TABLE sutta_pi (
+CREATE TABLE texts_pi (
 line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
 line_text TEXT NOT NULL,
-file_name TEXT NOT NULL);
+file_name TEXT NOT NULL,
+text_type TEXT NOT NULL);
 
-DROP TABLE IF EXISTS vinaya_pi;
-CREATE TABLE vinaya_pi (
+DROP TABLE IF EXISTS texts_var;
+
+CREATE TABLE texts_var (
 line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
 line_text TEXT NOT NULL,
-file_name TEXT NOT NULL);
+file_name TEXT NOT NULL,
+text_type TEXT NOT NULL);
 
-sutta_pi vinaya_pi sutta_var vinaya_var
-do 
-echo "
 
-DROP TABLE IF EXISTS $table;
-CREATE TABLE $table (
-line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
-line_text TEXT NOT NULL,
-file_name TEXT NOT NULL);" | sqlite3 $fdgdb
-done
+DROP TABLE IF EXISTS texts_en;
 
-DROP TABLE IF EXISTS sutta_ru;
-CREATE TABLE sutta_ru (
+CREATE TABLE texts_en (
 line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
 line_text TEXT NOT NULL,
 file_name TEXT NOT NULL,
 translator TEXT NOT NULL);
 
-DROP TABLE IF EXISTS sutta_en;
-CREATE TABLE sutta_en (
-line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
-line_text TEXT NOT NULL,
-file_name TEXT NOT NULL,
-translator TEXT NOT NULL);
 
-DROP TABLE IF EXISTS vinaya_en;
-CREATE TABLE vinaya_en (
+DROP TABLE IF EXISTS texts_ru;
+
+CREATE TABLE texts_ru (
 line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
 line_text TEXT NOT NULL,
 file_name TEXT NOT NULL,
-translator TEXT NOT NULL);
+translator TEXT NOT NULL,
+text_type TEXT NOT NULL);
 
 
 DROP TABLE IF EXISTS similes;
-
 
 CREATE TABLE similes (
 file_name TEXT PRIMARY KEY NOT NULL UNIQUE,
 metaphor_count INT NOT NULL DEFAULT 0,
 sg_count INT NOT NULL DEFAULT 0);
 
+DROP TABLE IF EXISTS text_names;
 
+CREATE TABLE text_names (
+line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+line_text TEXT NOT NULL,
+file_name TEXT NOT NULL);
+
+DROP TABLE IF EXISTS text_names_fullid;
+
+CREATE TABLE text_names_fullid (
+line_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+line_text TEXT NOT NULL,
+file_name TEXT NOT NULL);
 

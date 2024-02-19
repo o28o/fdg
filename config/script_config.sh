4f603d2853783cb6f63dbee07154158b0b216bc7
a9 <a9@sams.com>
2024-02-19 08:34:48 +0700

    updates

--- config/script_config.sh
+++ config/script_config.sh
@@ -47,8 +47,8 @@ multiplier=0.5
 skiprounds=3
 
 elif [[ "`uname -a`" == *"CentOS-Server"* ]];then
-#export LANG=C
-#export LC_ALL=C
+export LANG=C
+export LC_ALL=C
 #export LC_ALL=C.utf8
 #export LC_ALL=ru_RU.utf8
 #export LANG=ru_RU.utf8

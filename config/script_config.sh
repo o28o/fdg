06af7103113eed6f66f3555fc92f81916a423cac
o28 <o-flip5@flip5>
2024-02-23 07:33:44 +0700

    updates

--- config/script_config.sh
+++ config/script_config.sh
@@ -48,9 +48,9 @@ skiprounds=3
 
 elif [[ "`uname -a`" == *"CentOS-Server"* ]];then
 export LANG=C
-export LC_ALL=C
-#export LC_ALL=C.utf8
-export LC_ALL=ru_RU
+#export LC_ALL=C
+export LC_ALL=C.utf8
+#export LC_ALL=ru_RU.utf8
 #export LANG=ru_RU.utf8
 #export LANG=en_US.UTF-8
 #export LC_ALL=ru_RU.UTF-8 

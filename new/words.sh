dbe3943c7de17cddbb7c087500f41c1ee9458af0
a9 <a9@sams.com>
2024-02-24 09:20:32 +0700

    updates

--- new/words.sh
+++ new/words.sh
@@ -19,8 +19,6 @@ args="$@"
 source ./config/script_config.sh --source-only
 source ./new/functions.sh --source-only
 
-export LANG=C
-export LC_ALL=C
 keyword="$@"
 applyOutputLangToResponses
 [[ $keyword == "" ]] && exit 0
@@ -43,6 +41,8 @@ setSearchLanguage
 
 getWordsForCounts
 
+export LANG=C
+export LC_ALL=C
 if [ ! -s "$tmpdir/words" ]; then
 NotFoundErr
 #cd $apachesitepath > /dev/null

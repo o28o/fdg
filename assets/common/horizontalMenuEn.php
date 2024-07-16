abca3e308aad050f353c16f75f9ce4d0f0998725
ofold4 <o28o@mail.ru>
2024-07-16 06:55:27 -0400

    updates

--- assets/common/horizontalMenuEn.php
+++ assets/common/horizontalMenuEn.php
@@ -232,8 +232,16 @@ Memorizer</a></li>
     
    <li><a class="dropdown-item" target="_blank" href="/cse.php">Google Custom Search</a></li>
    
-      <li><a class="dropdown-item" target="_blank" href="https://norbu-ai.org/ebt/">Norbu AI</a></li>
-      <li><a class="dropdown-item" target="_blank" href="https://dharmamitra.org/">Mitra Translator</a></li>
+
+      <li>
+         <div class="dropdown-item ">
+      <a class="text-black" target="_blank" href="https://norbu-ai.org/ebt/">Norbu AI</a>
+
+         <a class="text-black" target="_blank" href="https://chatgpt.com">ChatGPT</a>
+  
+         </div>
+         </li>
+         <li><a class="dropdown-item" target="_blank" href="https://dharmamitra.org/">Mitra Translator</a></li>
 
    <li><a class="dropdown-item" target="_blank" href="https://Wisdomlib.org">Wisdomlib.org</a></li>
 
--- assets/common/horizontalMenuRu.php
+++ assets/common/horizontalMenuRu.php
@@ -241,7 +241,15 @@ echo '<!--
 
    <li><a class="dropdown-item" target="_blank" href="/cse.php">Google Custom Search</a></li>
 
-      <li><a class="dropdown-item" target="_blank" href="https://norbu-ai.org/ebt/">Norbu AI</a></li>
+
+      <li>
+         <div class="dropdown-item ">
+      <a class="text-black" target="_blank" href="https://norbu-ai.org/ebt/">Norbu AI</a>
+
+         <a class="text-black" target="_blank" href="https://chatgpt.com">ChatGPT</a>
+  
+         </div>
+         </li>
       <li><a class="dropdown-item" target="_blank" href="https://dharmamitra.org/">Mitra Переводчик</a></li>
 
               <li>

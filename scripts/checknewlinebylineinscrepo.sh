d8fa058b3268c55322b482216f9da166a75a8704
o28 <o-flip5@flip5>
2024-07-17 05:08:33 -0400

    updates

---
+++ scripts/checknewlinebylineinscrepo.sh
@@ -0,0 +1,7 @@
+cd /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data
+git pull
+cd /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/
+echo cp if have new sv files
+read x
+cp suttacentral.net/sc-data/sc_bilara_data/translation/ru/sv/sutta/sn/sn2/* assets/texts/sutta/sn/sn2/
+

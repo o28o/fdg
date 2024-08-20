55c83b611930038fb7b7ca6fa896de26d00258ac
a9 <a9@sams.com>
2024-08-19 21:28:09 -0400

    updates

--- scripts/checknewlinebylineinscrepo.sh
+++ scripts/checknewlinebylineinscrepo.sh
@@ -1,7 +1,33 @@
+#!/bin/bash
 cd /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data
-git pull
+git pull | grep "translation/ru"
 cd /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/
+
+
+Pali="/data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta"
+Trn="/data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data/sc_bilara_data/translation/ru"
+
+function ShowDiff() {
+cd $TrnDir
+TrnFolders=$(find . -type d | sort -V)
+
+for i in $TrnFolders ; do 
+echo $i r=$(ls $PaliDir/$i | wc -l) t=$(ls $TrnDir/$i | wc -l)
+#find $i -maxdepth 1 -type f | sort -V 
+done
+}
+
+echo KN
+PaliDir="$Pali/kn"
+TrnDir="$Trn/narinyanievmenenko/sutta/kn"
+ShowDiff
+
+echo SN
+# Задаем директории для перевода и палийских текстов
+PaliDir="$Pali/sn"
+TrnDir="$Trn/sv/sutta/sn"
+ShowDiff
+
 echo cp if have new sv files
 read x
 cp suttacentral.net/sc-data/sc_bilara_data/translation/ru/sv/sutta/sn/sn2/* assets/texts/sutta/sn/sn2/
-

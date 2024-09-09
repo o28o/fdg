0e73e5e903576fdd35709c451dc5a8015fd4ac41
o-kv <kvserv@o28o.zhh>
2024-09-10 03:43:15 +0500

    update from server

--- scripts/missingtranslations.sh
+++ scripts/missingtranslations.sh
@@ -12,9 +12,9 @@ do
 echo $sanyutta
 for i in `find  ../suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/$nikaya/$sanyutta/  -type f | awk -F'/' '{print $NF}' | sed 's/_.*//g' | sort -V | uniq ` ; do
 ls assets/texts/sutta/$nikaya/$sanyutta/${i}_* 2>&1 | grep "ls:" | awk -F'/' '{print $NF}' | sed 's/_.*/ missing/g' 
-done  | sort -V > total
-cat total
-wc -l total
+done  | sort -V > total_missing
+cat total_missing
+wc -l total_missing
 done 
 
-rm total
+rm total_missing

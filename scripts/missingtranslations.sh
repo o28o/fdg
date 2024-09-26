31dee72601f2a88e19c5973ccbe41daa47e4d55b
o yoga <o28o@mail.ru>
2024-09-26 10:51:48 -0400

    update from server

--- scripts/missingtranslations.sh
+++ scripts/missingtranslations.sh
@@ -11,10 +11,13 @@ for sanyutta in $snrange
 do
 echo $sanyutta
 for i in `find  ../suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/$nikaya/$sanyutta/  -type f | awk -F'/' '{print $NF}' | sed 's/_.*//g' | sort -V | uniq ` ; do
-ls assets/texts/sutta/$nikaya/$sanyutta/${i}_* 2>&1 | grep "ls:" | awk -F'/' '{print $NF}' | sed 's/_.*/ missing/g' 
+ls assets/texts/sutta/$nikaya/$sanyutta/${i}_* 2>&1 | grep "ls:" | awk -F'/' '{print $NF}' | sed 's/_.*/ missing/g' | tee out
 done  | sort -V > total_missing
 cat total_missing
 wc -l total_missing
 done 
 
 rm total_missing
+exit 0 
+
+for i in `find . -type f` ; do echo -n "`echo $i | sed 's@_root-pli-ms.json@@g'` `cat $i | wc -l`" ; echo ; done | sort -k2,2n -k1,1V

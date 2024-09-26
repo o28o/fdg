fd6f9648537c22932ed4a0aed3ed3bb479473715
o yoga <o28o@mail.ru>
2024-09-26 10:57:55 -0400

    update from server

---
+++ assets/js/lbl.js
@@ -0,0 +1,48 @@
+
+    // Функция для транслитерации пали в русские символы
+    function transliteratePali(title) {
+        const translitMap = {
+            "a": "а", "ā": "а", "i": "и", "ī": "и", "u": "у", "ū": "у",
+            "e": "э", "o": "о", "k": "к", "kh": "кх", "g": "г", "gh": "гх",
+            "c": "ч", "ch": "чх", "j": "дж", "jh": "джх", "ṭ": "т", "ṭh": "тх",
+            "ḍ": "д", "ḍh": "дх", "ṇ": "н", "t": "т", "th": "тх", "d": "д",
+            "dh": "дх", "n": "н", "p": "п", "ph": "пх", "b": "б", "bh": "бх",
+            "m": "м", "y": "й", "r": "р", "l": "л", "v": "в", "s": "с",
+            "ṣ": "ш", "h": "х"
+        };
+
+        // Заменяем пали на русский
+        let result = title.replace(/[a-zA-Zāīū]/g, (match) => translitMap[match] || match);
+        
+        return result.charAt(0).toUpperCase() + result.slice(1); // Делаем первую букву заглавной
+    }
+
+    // Функция для обработки названий глав
+    function processTitles(titles) {
+        return titles.map(title => {
+            // Удаляем номер и пробел
+            let cleanedTitle = title.replace(/^\d+\.\s*/, '').trim();
+
+            // Транслитерируем название
+            let transliteratedTitle = transliteratePali(cleanedTitle);
+            
+            // Возвращаем окончательное название
+            return `Глава ${transliteratedTitle}`;
+        });
+    }
+
+    // Пример названий глав
+    const titles = [
+        "7. Devatāvagga",
+        "10. Loṇakapallavagga",
+        "3. Sutta",
+        "5. Anussati"
+    ];
+
+    // Обработка названий глав
+    const processedTitles = processTitles(titles);
+    
+    console.log(processedTitles); // Вывод обработанных названий
+
+
+

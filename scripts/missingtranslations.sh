62fcef6563de4bea9f905077ab21d68d5378f0d5
o yoga <o28o@mail.ru>
2024-09-26 11:49:33 -0400

    update from server

--- scripts/missingtranslations.sh
+++ scripts/missingtranslations.sh
@@ -1,3 +1,34 @@
+
+if [[ "$@" == "" ]]  || [[ "$@" == "-n" ]] 
+then
+snrange=$(eval echo sn{1..56})
+nikaya=sn
+else
+snrange=$@
+nikaya=$(echo $snrange | sed 's/[0-9]//g' | sed 's/\s-n\s//g' )
+fi
+
+if [[ "$@" = *"-n"* ]]; then
+    sortMe="-k2,2n -k1,1V"  # по строкам
+else
+    sortMe="-k1,1V -k2,2n"  # по текстовому индексу
+fi
+
+for sanyutta in $snrange
+do
+echo $sanyutta
+#делаем список переводов
+find assets/texts/sutta/$nikaya/$sanyutta/ -type f | sed 's@_.*@_@g' | awk -F'/' '{print $NF}' | sort -V | uniq > trnList
+
+cat allRootTextWithLineCount | grep -E "$sanyutta\." | grep -v -f trnList  > missing
+cat missing | sort $sortMe
+wc -l missing
+
+rm missing trnList
+done 
+
+exit 0
+
 if [[ "$@" == "" ]]
 then
 snrange=$(eval echo sn{1..56})
@@ -20,4 +51,10 @@ done
 rm total_missing
 exit 0 
 
-for i in `find . -type f` ; do echo -n "`echo $i | sed 's@_root-pli-ms.json@@g'` `cat $i | wc -l`" ; echo ; done | sort -k2,2n -k1,1V
+
+
+
+for i in `find  ../suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/$nikaya/$sanyutta/  -type f | sort -V | uniq` ; do 
+echo -n "`echo $i | sed 's@_root-pli-ms.json@@g' | awk -F'/' '{print $NF}'` `cat $i | wc -l`" 
+echo  
+done | sort  -k2,2n -k1,1V

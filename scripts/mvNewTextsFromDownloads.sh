125c5903cbe512ec525bb02ce74de78ffd0d9a13
o-yoga <o28o@mail.ru>
2024-10-12 07:41:54 +0500

    updates

--- scripts/mvNewTextsFromDownloads.sh
+++ scripts/mvNewTextsFromDownloads.sh
@@ -3,6 +3,20 @@ source ./config/script_config.sh --source-only
 #downloaddir=/media/c/Users/o28o/Downloads
 #trndir=/media/c/soft/fdg/assets/texts/sutta/
 
+
+
+cd $downloaddir
+for file in `find .  -maxdepth 1 -type f -name "*root-pli-ms.json"`
+do
+
+newname=$(echo $file | sed 's@root-pli-ms@translation-ru-o@g')
+echo renamed $file $newname
+mv $file $newname
+done
+
+cd -  2>&1 >/dev/null
+
+
 for file in `find "$downloaddir" -maxdepth 1 -type f -name "*translation*.json"`
 do 
 suttaname=$(echo $file | sed -E 's/_translation.*//' | awk -F'/' '{print $NF}')
@@ -10,5 +24,5 @@ booknumber=$(echo $suttaname | sed -E 's/\..*//')
 nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
 
 mv $file $trndir/$nikaya/$booknumber/
-echo "$suttaname moved to ./$nikaya/$booknumber" 
+echo "moved $suttaname to ./$nikaya/$booknumber" 
 done

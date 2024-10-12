766fbdf32b5d889ae81f5fa505642a0b5caedb03
o-yoga <o28o@mail.ru>
2024-10-12 20:47:30 +0500

    updates

--- sc/styleforsc.sh
+++ sc/styleforsc.sh
@@ -58,6 +58,24 @@ echo " done <br>"
 done
 
 
+
+
+exit 0 
+
+
+if grep -qE 'ни-прият.*ни-болез' $i
+then
+echo -n "fixing adhukkhamasukha in $i"
+#fix adukkhamasukkha
+sed -i '/ни-прият.*ни-болез/s/ни-приятн/ни-буфер/g' $file
+sed -i '/ни-буфер.*ни-болез/s/ни-болезненн/ни-приятн/g' $file
+sed -i '/ни-буфер.*ни-прият/s/ни-буфер/ни-болезненн/g' $file
+echo " done <br>"
+fi
+
+#fix Учитель
+grep -Ei («Учитель|, Учитель) | grep -vi "счастлив" 
+
 #Hyphen joins words (station-master).
 #En-dash indicates range usually of numbers (AN 3.43–7)
 #Em-dash indicates a break in a sentence—like this.

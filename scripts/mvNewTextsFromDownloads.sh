cfcd5c43c07793b5d99d1b178f9ea020da502cb0
o-yoga <o28o@mail.ru>
2024-10-18 05:10:47 +0500

    updates

--- scripts/mvNewTextsFromDownloads.sh
+++ scripts/mvNewTextsFromDownloads.sh
@@ -20,8 +20,14 @@ cd -  2>&1 >/dev/null
 for file in `find "$downloaddir" -maxdepth 1 -type f -name "*translation*.json"`
 do 
 suttaname=$(echo $file | sed -E 's/_translation.*//' | awk -F'/' '{print $NF}')
+
+if [[ $suttaname =~ snp|iti|thig|thag|ud ]]; then
+    nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=vagga$(echo $suttaname | sed -E 's/\..*//' | sed 's/[a-z]*//g')
+else
 booknumber=$(echo $suttaname | sed -E 's/\..*//')
-nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+fi
 
 mv $file $trndir/$nikaya/$booknumber/
 echo "moved $suttaname to ./$nikaya/$booknumber" 

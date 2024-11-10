ff474233e88fe0b7650171f43638d6e560cb2a8a
a9 <a9@sams.com>
2024-11-10 08:10:34 +0800

    updates

--- scripts/mvNewTextsFromDownloads.sh
+++ scripts/mvNewTextsFromDownloads.sh
@@ -3,19 +3,15 @@ source ./config/script_config.sh --source-only
 #downloaddir=/media/c/Users/o28o/Downloads
 #trndir=/media/c/soft/fdg/assets/texts/sutta/
 
-
-
 cd $downloaddir
 for file in `find .  -maxdepth 1 -type f -name "*root-pli-ms.json"`
 do
-
 newname=$(echo $file | sed 's@root-pli-ms@translation-ru-o@g')
 echo renamed $file $newname
 mv $file $newname
 done
 
-cd -  2>&1 >/dev/null
-
+cd - 2>&1 >/dev/null
 
 for file in `find "$downloaddir" -maxdepth 1 -type f -name "*translation*.json"`
 do 
@@ -32,3 +28,33 @@ fi
 mv $file $trndir/$nikaya/$booknumber/
 echo "moved $suttaname to ./$nikaya/$booknumber" 
 done
+
+if [[ "`uname -a`" != *"Android"* ]]; then 
+exit 0
+fi
+
+audioSrcdir=/storage/emulated/0/Music/VoiceChangerWithEffects
+audioDestdir=/data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/audio
+
+for file in `find "$audioSrcdir" -maxdepth 1 -type f -name "*_*"`
+do 
+suttaname=$(echo $file | sed -E 's/_.*//' | awk -F'/' '{print $NF}')
+
+if [[ $suttaname =~ snp|iti|thig|thag|ud ]]; then
+    nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=vagga$(echo $suttaname | sed -E 's/\..*//' | sed 's/[a-z]*//g')
+elif [[ $suttaname =~ mn|dn ]]; then
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+booknumber=
+else
+booknumber=$(echo $suttaname | sed -E 's/\..*//')
+    nikaya=$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+fi
+newfn=$(echo $file | sed 's/_.*/_puriso_pali.mp3/g')
+mv "$file" $(echo $file | sed 's/_.*/_puriso_pali.mp3/g') >/dev/null 2>&1
+
+mv $newfn $audioDestdir/$nikaya/$booknumber/
+echo "moved $suttaname to audio/$nikaya/$booknumber" 
+done
+
+exit 0

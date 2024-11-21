524778c924981f51be4eeb0966257256898bd570
a9 <a9@sams.com>
2024-11-21 21:40:35 +0800

    updates

--- scripts/mvNewTextsFromDownloads.sh
+++ scripts/mvNewTextsFromDownloads.sh
@@ -4,7 +4,7 @@ source ./config/script_config.sh --source-only
 #trndir=/media/c/soft/fdg/assets/texts/sutta/
 
 cd $downloaddir
-for file in `find .  -maxdepth 1 -type f -name "*root-pli-ms.json"`
+for file in `find .  -maxdepth 1 -type f -size +0 -name "*root-pli-ms.json"`
 do
 newname=$(echo $file | sed 's@root-pli-ms@translation-ru-o@g')
 echo renamed $file $newname
@@ -13,12 +13,14 @@ done
 
 cd - 2>&1 >/dev/null
 
-for file in `find "$downloaddir" -maxdepth 1 -type f -name "*translation*.json"`
+#find "$downloaddir" -maxdepth 1 -type f -size 0 -name "*translation*.json" | xargs rm
+
+for file in `find "$downloaddir" -maxdepth 1 -type f -size +0 -name "*translation*.json"`
 do 
 suttaname=$(echo $file | sed -E 's/_translation.*//' | awk -F'/' '{print $NF}')
 
 if [[ $suttaname =~ snp|iti|thig|thag|ud ]]; then
-    nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
+nikaya=kn/$(echo "$suttaname" | sed -E 's/[0-9]+.*//')
 booknumber=vagga$(echo $suttaname | sed -E 's/\..*//' | sed 's/[a-z]*//g')
 else
 booknumber=$(echo $suttaname | sed -E 's/\..*//')
@@ -37,7 +39,7 @@ audioSrcdir=/storage/emulated/0/Music/VoiceChangerWithEffects
 audioSrcdir2='/storage/emulated/0/Recordings/Voice Recorder'
 audioDestdir=/data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/audio
 
-find "$audioSrcdir" "$audioSrcdir2" -maxdepth 1 -type f -name "*_*" | while IFS= read -r file 
+find "$audioSrcdir" "$audioSrcdir2" -maxdepth 1 -size +0 -type f -name "*_*" | while IFS= read -r file 
 do
 suttaname=$(echo "$file" | sed -E 's/_.*//' | awk -F'/' '{print $NF}')
 

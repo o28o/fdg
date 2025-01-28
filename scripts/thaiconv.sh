985672732f4ef278491785b7eaa8d2b482f7db0a
o28 <o-flip5@flip5>
2025-01-28 08:27:18 +0700

    updates

---
+++ scripts/thaiconv.sh
@@ -0,0 +1,77 @@
+###thai translit
+
+
+apachepath=/data/data/com.termux/files/usr/share/apache2/default-site/htdocs
+scroot=$apachepath/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms
+thairoot=$apachepath/assets/texts/th/root/pli/ms
+tmproot=$apachepath/tmp
+outputFileNameAddition=rootth-pli-ms.json
+
+find $tmproot -name "*_"  |sort -V | while read suttaid 
+do
+suttaid=$(echo $suttaid | awk -F/ '{print $NF}')
+echo $suttaid
+paste <(awk '{print $1}' < $(find $scroot -name "${suttaid}*")) <(awk -F': ' '{print $2}' < $tmproot/${suttaid} ) > $thairoot/sutta/${suttaid}${outputFileNameAddition}
+
+cat -tnv $thairoot/sutta/${suttaid}${outputFileNameAddition}
+
+cat $thairoot/sutta/${suttaid}${outputFileNameAddition} | jq 
+sleep 5
+done
+
+exit 0
+
+
+#check 
+find -type f | while read f ; do echo $f ; cj $f . > /dev/null; done
+
+
+$scroot
+find . -name "*.json" |sort -V | while read filename 
+
+done
+
+/sn/sn12/${suttaid}_rootth-pli-ms.json
+
+cd $scroot
+find . -name "*.json" |sort -V | while read filename 
+do
+echo $filename
+curl --get "http://aksharamukha-plugin.appspot.com/api/public" --data-urlencode "source=ISOPali" --data-urlencode "target=Thai" --data-urlencode "text=$(< $filename)"
+sleep 10
+done
+cd - 
+
+
+suttaid=sn56.11
+filename=$(find $scroot -name "${suttaid}_*")
+curl --get "http://aksharamukha-plugin.appspot.com/api/public" --data-urlencode "source=ISOPali" --data-urlencode "target=Thai" --data-urlencode "text=$(< $filename)"
+
+paste <(awk '{print $1}' < $(find $scroot -name "${suttaid}_*")) <(awk -F': ' '{print $2}' < $(find $thairoot -name "${suttaid}_*") ) > $thairoot/sutta/sn/sn12/${suttaid}_rootth-pli-ms.json
+
+
+
+
+curl "http://aksharamukha-plugin.appspot.com/api/public?source=ISOPali&target=Thai" --data-urlencode "text=$(< /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/an/an10/an10.46_root-pli-ms.json )" > an10.46_root-pli-mstmp.json 
+
+paste <(awk -F' ' '{print $1}' /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/an/an10/an10.46_root-pli-ms.json) <(awk -F'": ' '{print $2}' /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/texts/th/root/pli/ms/sutta/an/an10/an10.46_root-pli-mstmp.json ) > an10.46_rootth-pli-ms.json
+
+
+
+
+
+
+
+cd /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/sutta/dn/
+find . -name "*.json" |sort -V | while read filename 
+do
+echo $filename
+curl --get "http://aksharamukha-plugin.appspot.com/api/public" --data-urlencode "source=ISOPali" --data-urlencode "target=Thai" --data-urlencode "text=$(< $filename)"
+sleep 10
+done
+cd - 
+
+
+suttaid=sn12.2
+paste <(awk '{print $1}' < $(find /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/ -name "${suttaid}_*")) <(awk -F': ' '{print $2}' < $(find /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/texts/th/root/pli/ms -name "${suttaid}_*") ) > /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/assets/texts/th/root/pli/ms/sn/sn12/${suttaid}_rootth-pli-ms.json
+

c67858e1b1f9f5182a0235e1dd89be53a8018465
o28 <o-flip5@flip5>
2025-02-03 00:00:48 +0700

    updates

--- sc/multilang.js
+++ sc/multilang.js
@@ -426,10 +426,11 @@ if ((translator === 'sujato') || (translator === 'brahmali')) {
 
 const origUrl = window.location.href;
 let rvUrl = origUrl.replace("/ru/sc/", "/sc/");
+thUrl = origUrl.replace("/ru/sc/", "/th/sc/");
 rvUrl = rvUrl.replace("ml.html", "");
 rvUrl = rvUrl.replace("/sc/", "/sc/memorize.html");
 
-const warning = "<p class='warning'>Внимание!<br>Переводы, словари и комментарии<br>сделаны не Благословенным.<br>Сверяйтесь с Пали в 4 основных никаях.<a class='text-decoration-none' target='' href='" + rvUrl + "'>&nbsp;</a></p>";
+const warning = "<p class='warning'>Внимание!<br>Переводы, словари и комментарии<br>сделаны не Благословенным.<a style='cursor: pointer;' class='text-decoration-none' target='' href='" + thUrl + "'>&nbsp;</a><br>Сверяйтесь с Пали в 4 основных никаях.<a style='cursor: pointer;' class='text-decoration-none' target='' href='" + rvUrl + "'>&nbsp;</a></p>";
 
 //var lineBreak = "\n\n",
 //revhtml = html.split(lineBreak).reverse().join(lineBreak)

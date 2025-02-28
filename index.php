560f538d577dcae47ff65ea2d2d1830dbc39fcae
o28 <o-flip5@flip5>
2025-02-28 08:05:27 +0700

    updates

--- index.php
+++ index.php
@@ -1023,8 +1023,8 @@ foreach ($slides as $index => $slide) {
         </footer>
         <!-- Copyright Section-->
 <div class="copyright py-4 text-center text-white " >
-<div class="container">    <button class="btn btn-secondary text-center installButton" id="" style="display:none;"><?php echo $installpwa;?></button> <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Лицензия Creative Commons" style="border-width:0" src="/assets/img/88x31.png" loading="lazy" /></a> <small>Copyright <a class="text-white text-decoration-none" href="/assets/readylinebyline.html">&copy;</a> Dhamma.gift <?php echo $mode; ?> 
-<a class="text-white text-decoration-none" href="/assets/countdowntable.php">2022</a>-<?php echo date("Y"); ?></small></div>
+<div class="container"> <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Лицензия Creative Commons" style="border-width:0" src="/assets/img/88x31.png" loading="lazy" /></a> <small>Copyright <a class="text-white text-decoration-none" href="/assets/readylinebyline.html">&copy;</a> Dhamma.gift <?php echo $mode; ?> 
+<a class="text-white text-decoration-none" href="/assets/countdowntable.php">2022</a>-<?php echo date("Y"); ?></small>     <button class="btn btn-secondary text-center installButton" id="" style="display:none;"><?php echo $installpwa;?></button></div>
         </div>
         <!-- Portfolio Modals-->
 <script>

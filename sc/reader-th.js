dca1ed03b327360b646d6f74ce0292ea9b98a852
o28 <o-flip5@flip5>
2025-03-16 13:47:31 +0700

    updates

--- sc/reader-th.js
+++ sc/reader-th.js
@@ -101,7 +101,27 @@ var thtrnpath = `/assets/texts/${pathLang}/translation/${texttype}/${slugReady}_
 
 var theditedtrnpath = `/assets/texts/${pathLang}/translation/${texttype}/${slugReady}_translation-${pathLang}-${translator}+edited+o.json`;
 
-var rootpath = `/assets/texts/${pathLang}/root/pli/ms/${texttype}/${slugReady}_root${pathLang}-pli-ms.json`;
+
+let params = new URLSearchParams(document.location.search);
+ let script = params.get("script");
+ 
+   const savedScript = localStorage.getItem('selectedScript');
+   const siteLanguage = localStorage.getItem('siteLanguage');
+
+ if (( script === "devanagari" ) || ( savedScript === "Devanagari" ) ) {
+var rootpath = `/assets/texts/devanagari/root/pli/ms/${texttype}/${slugReady}_rootd-pli-ms.json`
+ } 
+ else if (( script === "thai" ) || ( savedScript === "Thai" ) || (siteLanguage === "th")) {
+var rootpath = `/assets/texts/th/root/pli/ms/${texttype}/${slugReady}_rootth-pli-ms.json`
+ } else if ( script === "isopali" ) {
+var rootpath = `${Sccopy}/sc-data/sc_bilara_data/root/pli/ms/${texttype}/${slugReady}_root-pli-ms.json`
+ } else {
+var rootpath = `${Sccopy}/sc-data/sc_bilara_data/root/pli/ms/${texttype}/${slugReady}_root-pli-ms.json`
+ }
+
+
+
+
 console.log('thai rootpath ' + rootpath);
 //rootpath = `${Sccopy}/sc-data/sc_bilara_data/root/pli/ms/${texttype}/${slugReady}_root-pli-ms.json`;
 

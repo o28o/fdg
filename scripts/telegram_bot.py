7f04929dfa331cb8de9af429570cb9c90f7133ee
Ubu kv <ubu@fdg.com>
2025-04-03 18:22:16 +0500

    updates

--- scripts/telegram_bot.py
+++ scripts/telegram_bot.py
@@ -1,11 +1,13 @@
+import json
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
-import requests
 
-TOKEN = "5702477766:AAH1wguJBwUNpy6nYkad9OiikBN6lb7HgPQ"
+# Загружаем токен из config.json
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    TOKEN = config.get("TOKEN", "")
 
 async def start(update: Update, context: CallbackContext):
-    # Сообщение при вводе /start
     await update.message.reply_text(
         "Добро пожаловать! Вот доступные команды:\n"
         "/find - Поиск на dhamma.gift\n"
@@ -20,35 +22,34 @@ async def find(update: Update, context: CallbackContext):
     if not context.args:
         await update.message.reply_text("Использование: /find слово, фраза или номер текста, как sn56.11")
         return
-    
+
     query = "+".join(context.args)
     url = f"https://dhamma.gift/ru/?q={query}"
-    
+
     await update.message.reply_text(f"Ищем: {query}\n[Результаты]({url})", parse_mode="Markdown")
 
 async def read(update: Update, context: CallbackContext):
     if not context.args:
         await update.message.reply_text("Использование: /read слово, фраза или номер текста, как sn56.11")
         return
-    
+
     query = "+".join(context.args)
     url = f"https://dhamma.gift/r/?q={query}"
-    
+
     await update.message.reply_text(f"Читаем: {query}\n[Результаты]({url})", parse_mode="Markdown")
 
 async def dict_search(update: Update, context: CallbackContext):
     if not context.args:
         await update.message.reply_text("Использование: /dict слово на пали, русском или английском")
         return
-    
+
     query = "+".join(context.args)
     url = f"https://dict.dhamma.gift/ru?q={query}"
-    
+
     await update.message.reply_text(f"Словарь: {query}\n[Результаты]({url})", parse_mode="Markdown")
 
 app = Application.builder().token(TOKEN).build()
 
-# Добавляем обработчики команд
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("find", find))
 app.add_handler(CommandHandler("read", read))
@@ -56,3 +57,6 @@ app.add_handler(CommandHandler("dict", dict_search))
 
 print("Бот запущен...")
 app.run_polling()
+
+
+

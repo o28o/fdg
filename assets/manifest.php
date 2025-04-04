1892c7abfe03b5e7075f009d425653eb1f9cfe52
o28 <o-flip5@flip5>
2025-04-05 05:06:58 +0700

    updates

--- assets/manifest.php
+++ assets/manifest.php
@@ -43,7 +43,7 @@ echo json_encode([
 
     // Поддержка Web Share Target API
     "share_target" => [
-        "action" => "/",
+        "action" => $start_url,
         "method" => "GET",
         "params" => [
             "text" => "q"

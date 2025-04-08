f819be4009a22c60e074f1c870c9d130a327e02c
o28 <o-flip5@flip5>
2025-04-08 15:45:51 +0700

    updates

--- scripts/telegram_bot/restart.sh
+++ scripts/telegram_bot/restart.sh
@@ -3,25 +3,27 @@
 restart() {
   local dir=$1
   local name=$2
-  local main=$name.py
+  local config=$3
   local tmp=/tmp/nohup_$name.out
 
-  echo "Restarting $main in $dir"
+  echo
+  echo "Restarting $name"
+  echo 
   cd "$dir" || exit 1
 
-  tail "$tmp"
-  rm "$tmp"
+tail "$tmp" 2>/dev/null
+rm "$tmp" 2>/dev/null
 
   echo "before kill"
-  ps -ef | grep "$main" | grep -v grep
+  ps -ef | grep telegram | grep $name | grep -v grep
 
-  pkill -f "$main"
-
-  nohup ./telegram/bin/python "$main" > "$tmp" 2>&1 &
+  pkill -f "$name"
+  nohup ./telegram/bin/python main.py "$config" > "$tmp" 2>&1 &
   echo "after nohup"
-  ps -ef | grep "$main" | grep -v grep
+  ps -ef | grep "$name" | grep -v grep
+  echo
   echo
 }
 
-restart /var/www/telegram_bot dgift
-restart /var/www/prod_bot bot
+restart /var/www/telegram_bot dgift_bot config.dgift_bot.json
+restart /var/www/telegram_bot dhammagift_bot config.dhammagift_bot.json

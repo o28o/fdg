42cbfa9ba86c0f68591fb18adefd3cfa2b97f865
o28 <o-flip5@flip5>
2025-04-20 20:46:16 +0500

    updates

--- scripts/ogg2mp3.sh
+++ scripts/ogg2mp3.sh
@@ -1,6 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
 for ogg in *.ogg; do
-    echo "Конвертирую: $ogg → ${ogg%.ogg}.mp3"
-    ffmpeg -i "$ogg" -acodec libmp3lame -q:a 2 "${ogg%.ogg}.mp3"
+    ffmpeg -i "$ogg" -acodec libmp3lame -q:a 4 -joint_stereo 1 "${ogg%.ogg}.mp3"
 done
+
 echo "Готово!"

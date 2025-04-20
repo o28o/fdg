d23c38c588822d2c1cfdfb2d10bda459a8994f03
o28 <o-flip5@flip5>
2025-04-20 19:23:21 +0500

    updates

--- assets/js/audioLazyLoad.js
+++ assets/js/audioLazyLoad.js
@@ -1,20 +1,4 @@
 
-function loadLazyAudio() {
-  var lazyAudios = document.querySelectorAll('.lazy-audio');
-  
-  lazyAudios.forEach(function(audio) {
-    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-    
-    if (isSafari) {
-      // Клонируем элемент и заменяем старый
-      var newAudio = audio.cloneNode(true);
-      audio.parentNode.replaceChild(newAudio, audio);
-      newAudio.load(); // Пробуем load() на новом элементе
-    } else {
-      audio.load(); // Для других браузеров оставляем как есть
-    }
-  });
-}
 
   // Вызов loadLazyAudio() в конце страницы или при необходимости
   // Например: <button onclick="loadLazyAudio()">Загрузить аудио</button>

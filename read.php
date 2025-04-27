dcf4f3df92ef33c1cce22321dc57a7fdf1d5f346
o-yoga <o28o@mail.ru>
2025-04-27 15:48:10 +0500

    updates

--- read.php
+++ read.php
@@ -61,6 +61,7 @@ if (isset($_GET['ml']) && $_GET['ml'] === 'on') {
 <link rel="icon" type="image/png" sizes="56x56" href="/assets/img/favicon-56x56.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+<link rel="icon" href="/assets/img/favicon.ico">
 
   <!-- Загрузка иконки для iOS  -->
   <link rel="apple-touch-icon" sizes="152x152" href="/assets/img/favico-noglass.png">

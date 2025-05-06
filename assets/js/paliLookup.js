141065929b343f67f40dc07ffb6e4e4d064893c1
o28o <90468182+o28o@users.noreply.github.com>
2025-05-06 07:58:51 +0500

    Update paliLookup.js

--- assets/js/paliLookup.js
+++ assets/js/paliLookup.js
@@ -282,12 +282,14 @@ function createPopup() {
         popup.style.left = savedLeft;
     }
 
-    const closeBtn = document.createElement('button');
-    closeBtn.classList.add('close-btn');
-    closeBtn.innerHTML = `
-    <img src="/assets/svg/xmark.svg" style="width:16px;height:16px;display:block;">
+const closeBtn = document.createElement('button');
+closeBtn.classList.add('close-btn');
+closeBtn.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="16" height="16" fill="currentColor">
+    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+  </svg>
 `;
-
+    
     // Создаем кнопку "Search with dhamma.gift"
     const openBtn = document.createElement('a');
     openBtn.classList.add('open-btn');

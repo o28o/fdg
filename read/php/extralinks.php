7262efe412df0e3411ee6b9e87f57ea48ae95488
o-yoga <o28o@mail.ru>
2025-05-06 15:07:11 +0500

    updates

--- read/php/extralinks.php
+++ read/php/extralinks.php
@@ -128,12 +128,9 @@ $playerHtml = "<span class='voice-dropdown'>
             <source src='$voicefile' type='$mimeType'>
             Your browser does not support the audio element.
         </audio><br>
-            <a href='javascript:void(0)' class='copy-pali'>Copy Pali</a> |
-            <a href='javascript:void(0)' class='copy-translation'>Copy Trn</a> |
+            Pali <a href='javascript:void(0)' class='copy-pali'>Copy</a> <a href='javascript:void(0)' class='open-pali'>Open</a> |
+			Trn <a href='javascript:void(0)' class='copy-translation'>Copy</a> <a href='javascript:void(0)' class='open-translation'>Open</a> |
             <a href='https://www.sc-voice.net/?src=sc#/sutta/$fromjs'>Voice.SC</a> |
-			<a href='javascript:void(0)' class='open-pali'>Open Pali</a> |
-			<a href='javascript:void(0)' class='open-translation'>Open Trn</a> |
-          
             <a href='/assets/common/ttsHelp.html'>?</a> 
     </span>
 </span>";

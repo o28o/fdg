479cd29898e755d6dcb3306081aa921e473a0173
o28 <o-flip5@flip5>
2025-05-14 05:38:59 +0500

    updates

--- scripts/multilang-search.php
+++ scripts/multilang-search.php
@@ -376,7 +376,7 @@ $check = ru2lat( $output );
 
 if ( preg_match('/(|-en)/', $p ) && ( preg_match('/(-not-in-|-net-v-)/', $check) ) && ( $p != "-vin" ) && ( $p != "-def" ))  {
   $fdgscript = "./new/finddhamma2.sh";
-$output = shell_exec("bash $fdgscript $outputlang $la -en $extra $cb $string");
+$output = shell_exec("bash $fdgscript $outputlang $la -en -kn $extra $cb $string");
 //                                                          		echo "<p>$output</p>";
       $output = trim(preg_replace('/\s\s+/', ' ', $output));	
 $outforjs .= $output . "<br>"; 
